a461e765908653e1a90cb3c1969b0a8e471dd36a
lykat <lykat@lykat.co.uk>
2021-01-25 18:23:17 +0000

    Add @ngrx/store and @ngrx/effects to project, as well as @ngrx/store-devtools which is only loaded in non-production environments.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {ModuleWithProviders, NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {StoreModule} from '@ngrx/store';
+import {EffectsModule} from '@ngrx/effects';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {environment} from '../environments/environment';
+
+function excludeFromProd<T>(module: ModuleWithProviders<T>): T[] | ModuleWithProviders<T> {
+  return environment.production ? [] : module;
+}
 
 @NgModule({
   declarations: [
@@ -10,7 +18,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+    excludeFromProd(StoreDevtoolsModule.instrument())
   ],
   providers: [],
   bootstrap: [AppComponent]
